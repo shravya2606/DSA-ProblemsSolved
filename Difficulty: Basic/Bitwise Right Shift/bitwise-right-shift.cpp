6866822e45da680bb6e0c65ed039355b7397fa1e
Shravya M <150315278+shravya2606@users.noreply.github.com>
2025-03-25 18:32:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bitwise Right Shift/bitwise-right-shift.cpp
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    // perform right shift here
+    
+    cout<< ( a >> b);
+
+    return 0;
+}
+
+
+//{ Driver Code Starts.
+
+// } Driver Code Ends
